e2ee42529c9aed7234c80e4bd5d04ebb4b429cd9
kush54 <115143746+kush54@users.noreply.github.com>
2024-07-05 04:43:39 -0700

    hoja bhais

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { addNewUser, removeUser, addupChatUsers, removeUnpairedUser, addupVideoUs
 const path = require("path");
 
 
-// app.use(express.static("public"));
+app.use(express.static("public"));
 app.use(express.static(path.resolve(__dirname,'build')));
 
 // git remote add origin https://github.com/kush54/hello.git
